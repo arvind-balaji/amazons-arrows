27915c34fb6913d4068ccd9e36bf19e1aedaaee3
arvind-balaji <arvindb02@gmail.com>
2019-03-31 00:56:06 -0500

    Refactor Move Class
     - added PotentialMove Class
     - score normalization
     - score weighting

---
+++ src/Move.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Move implements Comparable<Move> {
+  public int fromX;
+  public int fromY;
+  public int toX;
+  public int toY;
+  public int shootX;
+  public int shootY;
+
+  private List<Integer> scores;
+
+
+  public Move(int fromX, int fromY, int toX, int toY, int shootX, int shootY) {
+    this.fromX = fromX;
+    this.fromY = fromY;
+    this.toX = toX;
+    this.toY = toY;
+    this.shootX = shootX;
+    this.shootY = shootY;
+    this.scores = new ArrayList<>();
+  }
+
+  public int getScore() {
+    int total = 0;
+    for (int x : this.scores) {
+      total += x;
+    }
+    return total / scores.size();
+  }
+
+  public void addScore(int score) {
+    scores.add(score);
+  }
+
+  public List<Integer> getScores() {
+    return scores;
+  }
+
+  public void changeScore(int i, int score) {
+    this.scores.set(i, score);
+  }
+
+  public String getMoveString() {
+    return fromX + C.SPACE + fromY + C.SPACE + toX + C.SPACE + toY + C.SPACE + shootX + C.SPACE + shootY;
+  }
+
+  @Override
+  public String toString() {
+    return "Move{" +
+            "score=" + scores +
+            '}';
+  }
+
+  @Override
+  public int compareTo(Move o) {
+    return Integer.compare(this.getScore(), o.getScore());
+  }
+}
---
+++ src/PotentialMoves.java
@@ -0,0 +1,54 @@
+import java.util.Collections;
+import java.util.List;
+
+public class PotentialMoves {
+  private List<Move> moves;
+
+  public PotentialMoves(List<Move> moves) {
+    this.moves = moves;
+  }
+
+
+  public List<Move> getMoves() {
+    return moves;
+  }
+
+  public Move getBestMove() {
+    Collections.shuffle(moves);
+    Collections.sort(moves);
+    return moves.get(moves.size() - 1);
+  }
+
+  public void normalize() {
+    for (int i = 0; i < moves.get(0).getScores().size(); i++) {
+      normalizeMove(i, 0);
+    }
+  }
+
+  public void normalize(double[] weights) {
+    for (int i = 0; i < moves.get(0).getScores().size(); i++) {
+      normalizeMove(i, weights[i]);
+    }
+  }
+
+  public void normalizeMove(int index, double weight) {
+    double max = 0;
+    for (Move move : this.moves) {
+      int _score = (move.getScores().get(index));
+      max = _score > max ? _score : max;
+    }
+    for (Move move : this.moves) {
+      int normScore = (int) (((double) move.getScores().get(index) / max) * (1000 * weight));
+      move.changeScore(index, normScore);
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "PotentialMoves{" +
+            "moves=" + moves +
+            '}';
+  }
+
+
+}
